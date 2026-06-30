07c5608075196bcabe43793841063af1c3de7444
darkwulf <tech.darkwulf@gmail.com>
2026-06-30 17:28:19 +0200

    api/health endpoint of the server

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/darkwulf-T/Job-Analyzer/internal/handlers"
+)
+
+func main() {
+	mu := http.NewServeMux()
+
+	mu.HandleFunc("GET /api/health", handlers.HandlerReadiness)
+
+	server := http.Server{
+		Addr:    ":8000",
+		Handler: mu,
+	}
+	server.ListenAndServe()
+}
---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module github.com/darkwulf-T/Job-Analyzer
+
+go 1.26.1
